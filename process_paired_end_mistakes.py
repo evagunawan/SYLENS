8cd20743e5ae57dcaca10b4bacfeb1e59b6c3445
Eva Gunawan <gunawanevaa@gmail.com>
2023-09-11 11:10:17 -0500

    Added flipped filetypes to common mistakes

--- process_paired_end_mistakes.py
+++ process_paired_end_mistakes.py
@@ -7,6 +7,14 @@ def process_mistakes(firstR1, lastR1, firstR2, lastR2):
     logging.info('Checking for common errors.')
 
     #Errors if wrong files are supplied
+
+    if firstR1.endswith('2') and lastR1.endswith('2'):
+
+        if firstR2.endswith('1') and lastR2.endswith('1'):
+
+            logging.critical('Supplied files appear to be switched with Read 2 first and Read 1 second. Please flip files and restart Sylens. Program terminating...')
+            sys.exit(1)
+
     if firstR1.endswith('1') and lastR1.endswith('2'):
 
         if firstR2.endswith('1') and lastR2.endswith('2'):
